1b4f020adf876d179dc3ff0215c69ec872a6b499
AfzaalAbbasi <az.abbasi55@gmail.com>
2022-02-20 20:19:39 +0500

    JetPack Navigation initial blue print
    Main code

---
+++ app/src/main/java/com/afzaal/androidjetpacknavigation/fragments/SecondFragment.kt
@@ -0,0 +1,62 @@
+package com.afzaal.androidjetpacknavigation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.afzaal.androidjetpacknavigation.R
+import com.afzaal.androidjetpacknavigation.databinding.FragmentFirstBinding
+import com.afzaal.androidjetpacknavigation.databinding.FragmentSecondBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var binding: FragmentSecondBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentSecondBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            secondFragment = this@SecondFragment
+        }
+
+    }
+
+    fun goToNextScreen() {
+        findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+    }
+
+    fun cancel() {
+        findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+    }
+}
---
+++ app/src/main/java/com/afzaal/androidjetpacknavigation/fragments/ThirdFragment.kt
@@ -0,0 +1,49 @@
+package com.afzaal.androidjetpacknavigation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.afzaal.androidjetpacknavigation.R
+import com.afzaal.androidjetpacknavigation.databinding.FragmentFirstBinding
+import com.afzaal.androidjetpacknavigation.databinding.FragmentThirdBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class ThirdFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var binding: FragmentThirdBinding? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            thirdFragment = this@ThirdFragment
+        }
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentThirdBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+    fun goToNextScreen() {
+        findNavController().navigate(R.id.action_thirdFragment_to_startFragment)
+    }
+
+}
